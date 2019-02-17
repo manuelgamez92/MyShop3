9574f62351e9facb80c41ecf60ac9aaeb604d769
manue <manue@DESKTOP-PP7834V>
2019-02-17 23:29:39 +0100

    inicio

---
+++ MyShope.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyShope.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShope.Core.ViewModels
+{
+   public  class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
