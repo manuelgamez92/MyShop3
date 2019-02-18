afa90a7ae827e3832436cec2bb4cc4d0f8fc5f52
manue <manue@DESKTOP-PP7834V>
2019-02-17 17:30:20 -0700

    parte1

--- MyShope.Core/Models/Basket.cs
+++ MyShope.Core/Models/Basket.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace MyShope.Core.Models
 {
@@ -14,6 +15,7 @@ namespace MyShope.Core.Models
         {
             this.BasketItems = new List<BasketItem>();
         }
+        
 
     }
 }
